0eb972fb055f7698cc6b790319a9eeb2d9a8b1fe
kclamar <kclamar@connect.ust.hk>
2022-06-23 16:08:55 +0800

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="ztrack",
-        version="0.3.1",
+        version="0.3.2",
         entry_points={"console_scripts": ["ztrack=ztrack.cli:main"]},
         url="https://github.com/kclamar/ztrack",
     )
