afe8ff44282463964684b358c2ec5b7630ab2246
kclamar <kclamar@connect.ust.hk>
2022-06-23 15:45:17 +0800

    Add .mp4 support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="ztrack",
-        version="0.3.0",
+        version="0.3.1",
         entry_points={"console_scripts": ["ztrack=ztrack.cli:main"]},
         url="https://github.com/kclamar/ztrack",
     )
