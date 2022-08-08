cf873696bd7e8dca0a6f569243251670a5903a85
kclamar <kclamar@connect.ust.hk>
2022-08-08 15:33:09 +0800

    update tracking method

--- ztrack/tracking/tail/gradient2.py
+++ ztrack/tracking/tail/gradient2.py
@@ -2,12 +2,70 @@ from typing import Type
 
 import numpy as np
 import pandas as pd
-from scipy.ndimage import gaussian_filter1d
+from scipy.ndimage._filters import _gaussian_kernel1d, correlate1d  # noqa
 
 import ztrack.utils.cv as zcv
 from ztrack.tracking.tracker import Params, Tracker
 from ztrack.utils.shape import Line, Points
 from ztrack.utils.variable import Angle, Float, Int, Point
+
+
+def _track_img(
+    img: np.ndarray,
+    h,
+    w,
+    angle_rad,
+    tail_base,
+    sigma,
+    invert,
+    n_segments,
+    r,
+    half_lengths,
+    lengths,
+    weights,
+    roi=None,
+):
+    x, y = tail_base
+
+    if roi is not None:
+        x0, y0 = roi[:2]
+        x -= x0
+        y -= y0
+
+    if invert:
+        img = zcv.rgb2gray_dark_bg_blur(img, sigma, invert)
+
+    results = np.empty(n_segments)
+
+    for i in range(n_segments):
+        sin = np.sin(angle_rad)
+        cos = np.cos(angle_rad)
+
+        half_length = half_lengths[i]
+        length = lengths[i]
+        x0 = x + (r_cos := r * cos) - (l_sin := half_length * sin)
+        x1 = x + r_cos + l_sin
+        y0 = y + (r_sin := r * sin) + (l_cos := half_length * cos)
+        y1 = y + r_sin - l_cos
+
+        if min(x0, x1, y0, y1) >= 0 and max(x0, x1) < w and max(y0, y1) < h:
+            x_ = np.linspace(x0, x1, length).astype(int)
+            y_ = np.linspace(y0, y1, length).astype(int)
+
+            z = img[y_, x_]
+            z = correlate1d(
+                z.astype(float), weights, 0, mode="nearest", origin=0
+            )
+            m = length // 2
+            argmax = (z[:m].argmax() + m + z[m:].argmin()) // 2
+            angle_rad = np.arctan2(y_[argmax] - y, x_[argmax] - x)
+            x += round(r * np.cos(angle_rad))
+            y += round(r * np.sin(angle_rad))
+            results[i] = angle_rad
+        else:
+            results[i:] = np.nan
+
+    return results
 
 
 class GradientTailTracker2(Tracker):
@@ -27,9 +85,6 @@ class GradientTailTracker2(Tracker):
         raise NotImplementedError
 
     def _transform_from_roi_to_frame(self, results):
-        # if self.roi.value is not None:
-        #     x0, y0 = self.roi.value[:2]
-        #     results[:, :2] += (x0, y0)
         return results
 
     class __Params(Params):
@@ -60,7 +115,7 @@ class GradientTailTracker2(Tracker):
 
     @classmethod
     def _results_to_dataframe(cls, results):
-        return pd.DataFrame(results[:, :, -1])
+        return pd.DataFrame(results)
 
     def _track_img(self, img: np.ndarray):
         p = self.params
@@ -101,9 +156,13 @@ class GradientTailTracker2(Tracker):
                 y_ = np.linspace(y0, y1, length).astype(int)
 
                 z = img[y_, x_]
-                z = gaussian_filter1d(
-                    z.astype(float), sigma_tail, 0, 1, mode="nearest"
+
+                lw = int(4.0 * float(sigma_tail) + 0.5)
+                weights = _gaussian_kernel1d(sigma_tail, 1, lw)[::-1]
+                z = correlate1d(
+                    z.astype(float), weights, 0, mode="nearest", origin=0
                 )
+
                 m = length // 2
                 argmax = (z[:m].argmax() + m + z[m:].argmin()) // 2
                 angle = np.arctan2(y_[argmax] - y, x_[argmax] - x)
@@ -131,4 +190,61 @@ class GradientTailTracker2(Tracker):
         )
         self._line2.set_center_length_angle(
             a[-1, :2], self.params.w2, a[-1, -1] + np.pi / 2
+        )
+
+    def track_video(self, video_path, ignore_errors=False):
+        from decord import VideoReader
+        from tqdm import tqdm
+
+        self.set_video(video_path)
+
+        video_reader = VideoReader(str(video_path))
+
+        p = self.params
+        h, w = video_reader[0].shape[:2]
+        angle_rad = np.deg2rad(p.angle)
+        tail_base = p.tail_base
+        sigma = p.sigma
+        invert = p.invert
+        n_segments = p.n_segments
+        r = p.segment_length
+        half_lengths = np.linspace(p.w1, p.w2, n_segments)
+        lengths = (half_lengths * 2).astype(int)
+        sigma_tail = p.sigma_tail
+        roi = self.roi.value
+
+        it = (
+            tqdm(range(len(video_reader)))
+            if self._verbose
+            else range(len(video_reader))
+        )
+
+        s_ = self.roi.to_slice()
+
+        lw = int(4.0 * float(sigma_tail) + 0.5)
+        weights = _gaussian_kernel1d(sigma_tail, 1, lw)[::-1]
+
+        data = np.asarray(
+            [
+                _track_img(
+                    video_reader[i].asnumpy()[s_],
+                    h,
+                    w,
+                    angle_rad,
+                    tail_base,
+                    sigma,
+                    invert,
+                    n_segments,
+                    r,
+                    half_lengths,
+                    lengths,
+                    weights,
+                    roi,
+                )
+                for i in it
+            ]
+        )
+
+        return self._results_to_dataframe(
+            self._transform_from_roi_to_frame(data)
         )
