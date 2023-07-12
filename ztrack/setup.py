88b3b5819f050b699ca95cc9bdf344cc631281a4
kclamar <kclamar@connect.ust.hk>
2023-07-13 02:59:54 +0800

    add tail tracking method

--- ztrack/setup.py
+++ ztrack/setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="ztrack",
-        version="0.4.1",
+        version="0.5.0",
         entry_points={"console_scripts": ["ztrack=ztrack.cli:main"]},
         url="https://github.com/kclamar/ztrack",
     )
